345a1aba6d2176673cb125d51282b30a0fbcf81c
happy <k666kkk666k@163.com>
2026-06-14 22:55:44 +0800

    docs: 为geo包package-info添加中文注释喵~

--- src/main/java/io/github/thebusybiscuit/slimefun4/api/geo/package-info.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/api/geo/package-info.java
@@ -1,5 +1,6 @@
 /**
- * This package contains classes that are centered around the
- * {@link io.github.thebusybiscuit.slimefun4.api.geo.GEOResource} API.
+ * 本包包含所有围绕 GEOResource API 的类喵~
+ * GEOResource 是地理资源系统，用于在世界中扫描、存储和获取自定义资源（如矿石、稀有材料等）喵~
+ * 所有与地理资源相关的接口、枚举和工具类都放在这里喵~
  */
 package io.github.thebusybiscuit.slimefun4.api.geo;
