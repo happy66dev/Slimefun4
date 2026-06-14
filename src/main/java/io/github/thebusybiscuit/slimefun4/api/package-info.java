040038a8c8ea331d6f0cc52ef72aa00b79cd8da6
happy <k666kkk666k@163.com>
2026-06-14 23:17:32 +0800

    docs: 为api包package-info添加中文注释喵~

--- src/main/java/io/github/thebusybiscuit/slimefun4/api/package-info.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/api/package-info.java
@@ -1,5 +1,6 @@
 /**
- * This package contains a bunch of classes and sub-packages related to the interaction
- * with Slimefun via an API.
+ * 这个包里装着一堆和 Slimefun API 交互相关的类和子包喵~
+ * 包括但不限于：物品注册、事件系统、地理勘探、玩家数据、配方管理等功能入口喵~
+ * 外部插件想要和 Slimefun 打交道，基本上都要从这里找接口喵~
  */
 package io.github.thebusybiscuit.slimefun4.api;
