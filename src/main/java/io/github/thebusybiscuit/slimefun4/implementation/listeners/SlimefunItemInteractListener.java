3986aab34959f7b3725d4b20e96e07c3edd1f55c
happy <k666kkk666k@163.com>
2026-08-03 23:22:47 +0800

    chore: 增加右键事件调试日志喵~

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/SlimefunItemInteractListener.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/SlimefunItemInteractListener.java
@@ -16,6 +16,7 @@ import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
 import io.github.thebusybiscuit.slimefun4.utils.SlimefunUtils;
 import java.util.Optional;
+import java.util.logging.Level;
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 import me.mrCookieSlime.Slimefun.api.inventory.BlockMenuPreset;
@@ -52,6 +53,16 @@ public class SlimefunItemInteractListener implements Listener {
     @EventHandler
     public void onRightClick(PlayerInteractEvent e) {
         if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            Slimefun.logger().log(Level.INFO, "[右键Debug] Bukkit入口 玩家={0}, action={1}, hand={2}, item={3}, clickedBlock={4}, useBlock={5}, useItem={6}, cancelled={7}", new Object[] {
+                e.getPlayer().getName(),
+                e.getAction(),
+                e.getHand(),
+                e.getItem() == null ? "null" : e.getItem().getType(),
+                e.getClickedBlock() == null ? "null" : e.getClickedBlock().getType(),
+                e.useInteractedBlock(),
+                e.useItemInHand(),
+                e.isCancelled()
+            });
             // Exclude the Debug Fish here because it is handled in a seperate Listener
             if (SlimefunUtils.isItemSimilar(e.getItem(), SlimefunItems.DEBUG_FISH, true)) {
                 return;
@@ -69,6 +80,13 @@ public class SlimefunItemInteractListener implements Listener {
             Bukkit.getPluginManager().callEvent(event);
 
             boolean itemUsed = e.getHand() == EquipmentSlot.OFF_HAND;
+            Slimefun.logger().log(Level.INFO, "[右键Debug] 自定义事件后 玩家={0}, useItem={1}, useBlock={2}, itemUsed={3}, eventCancelled={4}", new Object[] {
+                e.getPlayer().getName(),
+                event.useItem(),
+                event.useBlock(),
+                itemUsed,
+                event.getInteractEvent().isCancelled()
+            });
 
             // Only handle the Item if it hasn't been denied
             if (event.useItem() != Result.DENY) {
@@ -115,6 +133,12 @@ public class SlimefunItemInteractListener implements Listener {
     @ParametersAreNonnullByDefault
     private boolean rightClickBlock(PlayerRightClickEvent event) {
         Optional<SlimefunItem> optional = event.getSlimefunBlock();
+        Slimefun.logger().log(Level.INFO, "[右键Debug] 方块处理入口 玩家={0}, clickedBlock={1}, slimefunBlock={2}, useBlock={3}", new Object[] {
+            event.getPlayer().getName(),
+            event.getClickedBlock().isPresent() ? event.getClickedBlock().get().getType() : "empty",
+            optional.isPresent() ? optional.get().getId() : "null",
+            event.useBlock()
+        });
 
         if (optional.isPresent()) {
             SlimefunItem sfItem = optional.get();
